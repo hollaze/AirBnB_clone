9ac46aca7e6dd951c82905cbf20f97c49e1f246d
hollaze <deperiers.a@gmail.com>
2021-06-24 14:56:43 +0200

    Need modification for 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,13 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         
-        if kwargs is not None:
-            self.__dict__.update(kwargs) 
+
         
-        else:
+        if kwargs is None:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            
+        #need else other condition
+        #+ it does not print name + my_number at last test
 
     def __str__(self):
         return "[{}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -26,10 +26,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
         key = self.__class__.__name__
         for key in self.__dict__.keys():
             return self.__dict__
-
