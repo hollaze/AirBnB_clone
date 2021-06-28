cb2e741f4b365f61ecb77347f6247853b9669f65
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-28 18:23:14 +0200

    Update the update console command

--- models/place.py
+++ models/place.py
@@ -14,7 +14,8 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = [str]
+    if str:
+        amenity_ids = []
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
