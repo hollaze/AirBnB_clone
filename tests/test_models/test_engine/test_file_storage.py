412441eeb2d812314bd8846c82fffc8baadf36c8
hollaze <deperiers.a@gmail.com>
2021-06-30 05:09:01 +0200

    uncompleted file

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from models.engine.file_storage import FileStorage
+import models
+import unittest
+
+
+class Test_FileStorage(unittest.TestCase):
+    def test_if_exist(self):
+        self.assertEqual(models.FileStorage, type(models.FileStorage()))
+    
+    def test__file_path(self):
+        filestorage = models.FileStorage()
+        self.assertEqual(str, type(filestorage._FileStorage__file_path))
+    
+    def test__objects(self):
+        filestorage = models.FileStorage()
+        self.assertEqual(dict, type(filestorage._FileStorage__objects))
+        
+    def test_all(self):
+        filestorage = models.FileStorage()
+        self.assertGreater(len(filestorage._FileStorage__objects), 0)
+        
+    def test_new(self):
+        filestorage = models.FileStorage()
+        self.assertIn()
+        
+if __name__ == '__main__':
+    unittest.main()
